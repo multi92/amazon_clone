094033bf23d967b1f9a99285bf4f9edf160bc4e3
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-09-19 10:39:39 +0200

    Product.js added

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -10,6 +11,24 @@ function Home() {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
         />
+
+        <div className="home__row">
+          <Product
+            id="12321341"
+            title="The lean Startup: How Constant Innovation Creates Redically Successful Business Paperbak"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
+
+          <Product
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
       </div>
     </div>
   );
