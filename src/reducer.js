1bc81b4dd009d281646b5636722e14a28712acd3
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-09-22 12:20:34 +0200

    reducer dispatch update

--- src/reducer.js
+++ src/reducer.js
@@ -17,7 +17,19 @@ const reducer = (state, action) => {
       };
     case "REMOVE_FROM_BASKET":
       //logic for removing item from basket
-      return { ...state };
+      //we cloned the basket
+      let newBasket = [...state.basket];
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its no in basket `
+        );
+      }
+      return { ...state, basket: newBasket };
     default:
       return state;
   }
