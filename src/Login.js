731b74c56ed4a927de8fea5739dee0c552fe2fae
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-09-29 10:28:56 +0200

    Login.js update

--- src/Login.js
+++ src/Login.js
@@ -18,7 +18,7 @@ function Login() {
       })
       .catch((error) => alert(error.message));
   };
-  //REGISTER
+  //REGISTER logic
   const register = (e) => {
     e.preventDefault(); //stop refreshing
     auth
